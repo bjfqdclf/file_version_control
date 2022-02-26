6576e050e7c9e7061ec7ab6c6cd0706a56e3ca49
leianhua <leianhua1@flkj.ai>
2022-02-26 15:12:28 +0800

    V2.1

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ import zip_file
 if __name__ == '__main__':
     path = os.path.dirname(os.path.realpath(sys.argv[0]))
     file_list = os.listdir(path)
-    file_list.remove(os.path.basename(sys.argv[0]))  # 去除当前可执行文件
+    ignore_file = ['.version', os.path.basename(sys.argv[0])]
     if '.version' in file_list:  # 去除生成文件
         file_list.remove('.version')
     else:
@@ -12,9 +12,10 @@ if __name__ == '__main__':
     date = datetime.datetime.now()
     date_str = f'{date.year}_{date.month}{date.day}_{date.hour}_{date.minute}_{date.second}.zip'
     zip_file_name = os.path.join(path, f'.version/{date_str}')
-    zip_file.zip_files(file_list, zip_file_name)
+    zip_file.zip_files(path, zip_file_name)
     print('>>> 备份完成...')
-    time.sleep(2)
+    time.sleep(1.5)
+
 
 
 
--- zip_file.py
+++ zip_file.py
@@ -1,9 +1,18 @@
-import zipfile
+import zipfile, os, sys
 
 
-def zip_files(files, zip_name):
-    zip = zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED)
-    for file in files:
-        print(f'zipping {file}...')
-        zip.write(file)
-    zip.close()
+def zip_files(start_dir, file_news):
+    start_dir = start_dir  # 要压缩的文件夹路径
+    z = zipfile.ZipFile(file_news, 'w', zipfile.ZIP_DEFLATED)
+    for dir_path, dir_names, file_names in os.walk(start_dir):
+        if os.path.basename(sys.argv[0]) in file_names:     # 跳过本文件
+            file_names.remove(os.path.basename(sys.argv[0]))
+        if dir_path == os.path.join(start_dir, '.version'):
+            continue
+        f_path = dir_path.replace(start_dir, '')  # 这一句很重要，不replace的话，就从根目录开始复制
+        f_path = f_path and f_path + os.sep or ''  # 实现当前文件夹以及包含的所有文件的压缩
+        for filename in file_names:
+            print(f'>>> {filename}...')
+            z.write(os.path.join(dir_path, filename), f_path + filename)
+    z.close()
+    return file_news
